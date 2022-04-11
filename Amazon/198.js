b4f323f4ab91977836d86e15b9bcdf290caded46
Jianshuo Yang <jyang63@stevens.edu>
2022-04-10 20:49:14 -0400

    dp bottom-up

--- Amazon/198.js
+++ Amazon/198.js
@@ -1,19 +1,10 @@
 var rob = function (nums) {
   // dp bottom-up
-
-  // base cases
-  if (!nums.length) return 0;
-  if (nums.length === 1) return nums[0];
-  if (nums.length === 2) return Math.max(nums[0], nums[1]);
-
-  let oneGap = nums[0];
-  let zeroGap = Math.max(nums[0], nums[1]);
-
+  const dp = [];
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
   for (let i = 2; i < nums.length; i++) {
-    const maxCurrent = Math.max(nums[i] + oneGap, zeroGap);
-    oneGap = zeroGap;
-    zeroGap = maxCurrent;
+    dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
   }
-
-  return zeroGap;
+  return dp[nums.length - 1];
 };
