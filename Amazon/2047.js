7b347384c836de5fea2add6b6b27030c6e35974c
Jianshuo Yang <jyang63@stevens.edu>
2022-04-28 17:11:37 -0400

    day 21

---
+++ Amazon/2047.js
@@ -0,0 +1,14 @@
+var countValidWords = function (sentence) {
+  let list = sentence.split(' ');
+  let filtered = list.filter((s) => {
+    if (/\d/.test(s) || s == '') return false; //removes anything with numbers or is blank
+    if (/^[!,.]$/.test(s)) return true; //punctuation only
+    if (/^\w+[!,.]?$/.test(s)) return true; //word + optional punctuation
+    if (/^\w+[-]?\w+[!,.]?$/.test(s)) return true; //word + optional hypen + word + optional punctuation
+    return false;
+  });
+  return filtered.length;
+};
+
+// \w -> [a-zA-Z0-9_]
+// \d -> [0-9]
