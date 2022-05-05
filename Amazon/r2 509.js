ca08eabcbd87634329ea14e2d7f16af67a87f392
Jianshuo Yang <jyang63@stevens.edu>
2022-05-05 18:26:07 -0400

    day 27

--- Amazon/r2 509.js
+++ Amazon/r2 509.js
@@ -1,8 +1,8 @@
 const fib = (n) => {
-  const dp = [];
+  const dp = new Array(n + 1);
   dp[0] = 0;
   dp[1] = 1;
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i < dp.length; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
   }
   return dp[n];
