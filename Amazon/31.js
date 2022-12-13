b51e6c7cb3223dafe64f21976298b8bb015d187f
Jianshuo Yang <jyang63@stevens.edu>
2022-12-13 18:43:26 -0500

    Dec. 13 back to depressing leetcode...

---
+++ Amazon/31.js
@@ -0,0 +1,25 @@
+const nextPermutation = (nums) => {
+  let idx = nums.length - 1;
+  while (nums[idx] <= nums[idx - 1]) idx--;
+
+  let left = idx - 1,
+    right = nums.length - 1;
+
+  while (nums[left] >= nums[right]) right--;
+
+  if (left >= 0 && right >= 0) {
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+  }
+
+  left = idx;
+  right = nums.length - 1;
+  while (left < right) {
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+    left++;
+    right--;
+  }
+};
+
+const arr = [1, 2, 3, 6, 5, 4, 2, 1];
+nextPermutation(arr);
+console.log(arr);
